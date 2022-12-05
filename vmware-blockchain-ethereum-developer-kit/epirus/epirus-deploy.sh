9bb25613e3e9e4743df6f04d5fdd4d698eddaee1
shivam-devops <shivam@web3labs.com>
2022-12-05 16:36:11 +0530

    epirus-deploy updated
    Signed-off-by: shivam-devops <shivam@web3labs.com>

--- vmware-blockchain-ethereum-developer-kit/epirus/epirus-deploy.sh
+++ vmware-blockchain-ethereum-developer-kit/epirus/epirus-deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 kubectl create namespace vmbc-epirus
 NAMESPACE="vmbc-epirus"
-
 
 cp epirus-net-networkpolicy.yml.tmpl epirus-net-networkpolicy.yml
 cp api-deployment.yml.tmpl api-deployment.yml
@@ -10,4 +8,13 @@ cp ingestion-deployment.yml.tmpl ingestion-deployment.yml
 cp mongodb-deployment.yml.tmpl mongodb-deployment.yml
 cp web-deployment.yml.tmpl web-deployment.yml
 
-kubectl apply -f api-deployment.yml,epirus-net-networkpolicy.yml,ingestion-deployment.yml,mongodb-deployment.yml,web-deployment.yml
+kubectl apply -f epirus-net-networkpolicy.yml,mongodb-deployment.yml,web-deployment.yml
+sleep 10
+name=$(kubectl get pods | grep mongo | awk -F '1/1' '{print $1}')
+ip=$(kubectl describe pod $name | awk -F 'IP:               ' '{print $2}')
+echo $name
+echo $ip
+latest=$(echo $ip)
+sed -i "s|172.17.0.5|$latest|g" "ingestion-deployment.yml"
+sed -i "s|172.17.0.5|$latest|g" "api-deployment.yml"
+kubectl apply -f api-deployment.yml,ingestion-deployment.yml
